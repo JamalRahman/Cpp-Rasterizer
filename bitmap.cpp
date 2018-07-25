6935d63d4b87099eceab7941889b458fd9d6d109
Jamal <jamalrahman95@gmail.com>
2018-07-25 01:50:34 +0100

    bitmap.cpp can generate an image using a 3d int array of data values

--- bitmap.cpp
+++ bitmap.cpp
@@ -1,2 +1,88 @@
 #include "bitmap.h"
+#include <stdio.h>
 
+const int defaultHeaderSize = 54;       // Header default uses the "BITMAPINFOHEADER" format, being 14 + 40 bytes long
+const int defaultBytesPerPixel = 3;     // Default colour depth is 24 bits per pixel. 24/8 = 3 bytes per pixel (rgb each 3 char digits long, 0-255))
+
+void saveBitmap(unsigned char *imageData, int height, int width, char *imageName);
+unsigned char* generateBitmapHeader(int height, int width);
+
+// Function Declarations    ----------------------------------------------------------------------
+
+void saveBitmap(unsigned char *imageData, int height, int width, char *imageName){
+
+    unsigned char* imageHeader = generateBitmapHeader(height, width);
+
+    // Due to bitmap's image data requiring all rows explained in multiples of 4 byte DWORDS, we determine how many bytes short our rows are of completing a DWORD, and will later pad them
+    unsigned char* paddingStructure[3] = {0,0,0};
+    int paddingSize = (4 -(width*defaultBytesPerPixel % 4))%4;
+
+    // We open the file
+    FILE* imageFile = fopen(imageName, "wb");
+
+    // Write header to file
+    fwrite(imageHeader,1,defaultHeaderSize,imageFile);
+
+    // Iterate over rows, writing each time
+    int index;
+    for(index=0; index<height; index++){
+        fwrite(imageData+(index*width*defaultBytesPerPixel),1,width*defaultBytesPerPixel,imageFile);
+        fwrite(paddingStructure,1,paddingSize,imageFile);
+    }
+
+    fclose(imageFile);
+
+}
+
+unsigned char* generateBitmapHeader(int height, int width){
+    
+    // Size of file = bytes in header + bytes in image data
+    int fileSize = defaultHeaderSize +defaultBytesPerPixel*height*width;
+
+    // Declaring 54 char long header data. See BMP specification
+    static unsigned char header[] = {      
+        0,0,        // BM
+        0,0,0,0,    // Size of bm file
+        0,0,0,0,    // Reserved - Unused
+        0,0,0,0,    // File offset index
+        0,0,0,0,    // Info header size ( 4o bytes)
+        0,0,0,0,    // Width (signed int)
+        0,0,0,0,    // Height (signed int)
+        0,0,        // Number of Colour planes
+        0,0,        // Num bits per pixel (colour depth)
+        0,0,0,0,    // Compression level
+        0,0,0,0,    // Image size
+        0,0,0,0,    // Horiz resolution
+        0,0,0,0,    // Vert res
+        0,0,0,0,    // Colour number
+        0,0,0,0,    // Number of important colours
+    };
+
+    // Filling in header data
+
+    header[0] = (unsigned char)'B';
+    header[1] = (unsigned char)'M';
+
+    header[2] = (unsigned char)(fileSize);
+    header[3] = (unsigned char)(fileSize>>8);
+    header[4] = (unsigned char)(fileSize>>16);
+    header[5] = (unsigned char)(fileSize>>24);
+
+    header[10] = (unsigned char)(defaultHeaderSize);
+    header[14] = (unsigned char)(40);
+
+    header[18] = (unsigned char)(width);
+    header[19] = (unsigned char)(width>>8);
+    header[20] = (unsigned char)(width>>16);
+    header[21] = (unsigned char)(width>>24);
+
+    header[22] = (unsigned char)(height);
+    header[23] = (unsigned char)(height>>8);
+    header[24] = (unsigned char)(height>>16);
+    header[25] = (unsigned char)(height>>24);
+
+    header[26] = (unsigned char)(1);
+    header[28] = (unsigned char)(defaultBytesPerPixel*8);
+
+    return header;
+}
--- main.cpp
+++ main.cpp
@@ -1,68 +1,9 @@
 #include <iostream>
 #include "bitmap.h"
 
-
-// Struct Definitions -------------------------------------------------------
-
-struct Vertex{
-    float x,y,z;
-    Vertex(float X, float Y, float Z){
-        x = X;
-        y = Y;
-        z = Z;
-    }
-};
-
-struct Vector{
-    float x,y,z;
-    Vector(float X, float Y, float Z){
-        x = X;
-        y = Y;
-        z = Z;
-    }
-};
-
-struct Triangle{
-    Vertex *v1;
-    Vertex *v2;
-    Vertex *v3;
-    Triangle(Vertex* V1, Vertex* V2, Vertex* V3){
-        v1 = V1;
-        v2 = V2;
-        v3 = V3;
-    }
-};
-
-struct Edge{
-    Vertex *v1;
-    Vertex *v2;
-    Edge(Vertex* V1, Vertex* V2){
-
-    }
-};
-
-struct Camera{
-    Vertex *pos;
-    Vector *direction;
-    Camera(Vertex* POS, Vector* DIRECTION){
-        pos = POS;
-        direction = DIRECTION;
-    }
-};
-
 // Function Definitions -------------------------------
 
 int main(int argc, char const *argv[]){
-    Vertex *v1 = new Vertex(0,0,0);
-    Vertex *v2 = new Vertex(1,1,0);
-    Vertex *v3 = new Vertex(1,0,0);
-    Vertex *cameraPos = new Vertex(0,0,0);
-    Vector *cameraDirection = new Vector(0,0,1);
-
-    Camera cam = new Camera(cameraPos,cameraDirection);
-    Triangle *t1 = new Triangle(v1,v2,v3);
-    Edge *e1 = new Edge(v1,v2);
-
 
     return 0;
 }
