e66959ba464399788f52ed90f4a63a4a5b7ca9f2
Jamal <jamalrahman95@gmail.com>
2018-07-25 02:06:28 +0100

    Made a header for the bitmap module

--- bitmap.cpp
+++ bitmap.cpp
@@ -1,13 +1,7 @@
 #include "bitmap.h"
 #include <stdio.h>
 
-const int defaultHeaderSize = 54;       // Header default uses the "BITMAPINFOHEADER" format, being 14 + 40 bytes long
-const int defaultBytesPerPixel = 3;     // Default colour depth is 24 bits per pixel. 24/8 = 3 bytes per pixel (rgb each 3 char digits long, 0-255))
-
-void saveBitmap(unsigned char *imageData, int height, int width, char *imageName);
-unsigned char* generateBitmapHeader(int height, int width);
-
-// Function Declarations    ----------------------------------------------------------------------
+// Functions    ----------------------------------------------------------------------
 
 void saveBitmap(unsigned char *imageData, int height, int width, char *imageName){
 
