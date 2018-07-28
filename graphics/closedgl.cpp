1c06e4016650b852f31f7b4e5367d59c6efd36a1
Jamal <jamalrahman95@gmail.com>
2018-07-28 20:10:30 +0100

    I cry every time

--- graphics/closedgl.cpp
+++
@@ -1,8 +0,0 @@
-//  closedgl.cpp
-//  ClosedGL - The Closed Graphics Library by Jamal Rahman 
-//
-//  Copyright (c) 2018 Jamal Rahman
-//  Use of this source code is governed by the MIT license that can be
-//  found in the LICENSE file.
-
-#include "closedgl.h"
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "bitmap.h"
 
 int main(int argc, char const *argv[]){
-    
+    Bitmap bitmap;
+    bitmap.readFile("flag.bmp");
+    bitmap.writeFile("testOutput.bmp");
     return 0;
 }
