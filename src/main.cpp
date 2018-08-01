9b8845781134f2bfaa5330103e0f90437739dee3
jamalrahman <jamalrahman95@gmail.com>
2018-08-01 21:26:16 +0100

    Completed Bresenham's Algo

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,16 @@
 
 
 int main(int argc, char const *argv[]){
-    drawLine(25,25,150,40);
+    drawLine(100,100,140,120);
+    drawLine(100,100,120,140);
+    drawLine(100,100,60,120);
+    drawLine(100,100,80,140);
+    
+    drawLine(100,100,140,80);
+    drawLine(100,100,120,60);
+    drawLine(100,100,60,80);
+    drawLine(100,100,80,60);
+
     saveImage();
     
     return 0;
--- src/renderEngine.cpp
+++ src/renderEngine.cpp
@@ -3,21 +3,55 @@
 Bitmap bitmap(200,200,24);
 BitmapColor red(255,0,0);
 
-void drawLine(int x0, int y0, int x1, int y1){
+void drawLineX(int x0, int y0, int x1, int y1){
     int e = 0,
         y = y0,
+        iterator=1,
         dx = x1-x0,
         dy=y1-y0;
+        if(dy<0){
+            dy = -dy;
+            iterator = -1;
+        }
     for(int x = x0;x<=x1;x++){
         plotPoint(x,y);
         e+=dy;
         if((2*(e))>=dx){
             e-=dx;
-            y++;
+            y+=iterator;
+        }
+    }
+}
+void drawLineY(int x0, int y0, int x1, int y1){
+    int e = 0,
+        x = x0,
+        iterator=1,
+        dx = x1-x0,
+        dy=y1-y0;
+        if(dx<0){
+            dx = -dx;
+            iterator = -1;
+        }
+    for(int y = y0;y<=y1;y++){
+        plotPoint(x,y);
+        e+=dx;
+        if((2*(e))>=dy){
+            e-=dy;
+            x+=iterator;
         }
     }
 }
 
+void drawLine(int x0, int y0, int x1, int y1){
+    if(abs(x0-x1)>abs(y0-y1)){
+        if(x0<x1) drawLineX(x0,y0,x1,y1);
+        else drawLineX(x1,y1,x0,y0);
+    }
+    else{
+        if(y0<y1) drawLineY(x0,y0,x1,y1);
+        else drawLineY(x1,y1,x0,y0);
+    }
+}
 void plotPoint(int x,int y){
     bitmap.set(x,y,red);
 }
